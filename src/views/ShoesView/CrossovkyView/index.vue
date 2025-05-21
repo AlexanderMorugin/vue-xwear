bafb1a70eeac9f1220b0b77358d59cca0fd67cc3
Alexander Morugin <nobilis@bk.ru>
2025-05-21 17:10:59 +0300

    стилизовал чекбоксы

--- src/views/ShoesView/CrossovkyView/index.vue
+++ src/views/ShoesView/CrossovkyView/index.vue
@@ -1,52 +1,64 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <!-- CrossovkyView -->
 <template>
-  <AppBreadcrumbs :breadcrumbs="breadcrumbs" />
-  <app-page class="container">
-    <app-left>
-      <!-- <AppFilterBox
+  <AppLoader v-if="isLoading" />
+  <div v-else>
+    <AppBreadcrumbs :breadcrumbs="breadcrumbs" />
+    <app-page class="container">
+      <app-left>
+        <!-- <AppFilterBox
         title="Бренды"
         :brandList="brandList.value"
         v-model:modelValue="selectedBrands"
       /> -->
 
-      <!-- Фильтр по брендам -->
-      <div class="filter-box">
-        <p class="filter-title">Бренды</p>
-        <ul class="filter-checkbox-list">
-          <li class="filter-checkbox-item" v-for="(item, index) in brandList.value" :key="index">
-            <input type="checkbox" :id="item" :value="item" v-model="selectedBrands" />
-            <label for="item">{{ item }}</label>
-          </li>
-        </ul>
-      </div>
+        <!-- Фильтр по брендам -->
+        <div class="filter-box">
+          <p class="filter-title">Бренды</p>
+          <ul class="filter-checkbox-list">
+            <li class="filter-checkbox-item" v-for="(item, index) in brandList.value" :key="index">
+              <input
+                type="checkbox"
+                :id="item"
+                :value="item"
+                v-model="selectedBrands"
+                class="checkbox-input"
+              />
+              <label for="item" class="checkbox-label">{{ item }}</label>
+            </li>
+          </ul>
+        </div>
 
-      <!-- Фильтр по цвету -->
-      <div class="filter-box">
-        <p class="filter-title">Цвет</p>
-        <ul class="filter-checkbox-list">
-          <li class="filter-checkbox-item" v-for="(item, index) in colorList.value" :key="index">
-            <input type="checkbox" :id="item" :value="item" v-model="selectedColor" />
-            <label for="item">{{ item }}</label>
-          </li>
-        </ul>
-      </div>
-    </app-left>
-    <app-right>
-      <AppHeading
-        title="Кроссовки"
-        :quantity="filterData.length"
-        sortBox="true"
-        @toggleSorting="toggleSorting"
-        :isDesc="isDesc"
-      />
-      <!-- <AppProductList :shoes="sortProductByPrice(filterData)" /> -->
-      <!-- <AppProductList :shoes="filterBrandData" /> -->
-      <AppProductList :shoes="sortProductByPrice(filterData)" />
+        <!-- Фильтр по цвету -->
+        <div class="filter-box">
+          <p class="filter-title">Цвет</p>
+          <ul class="filter-checkbox-list">
+            <li class="filter-checkbox-item" v-for="(item, index) in colorList.value" :key="index">
+              <input
+                type="checkbox"
+                :id="item"
+                :value="item"
+                v-model="selectedColor"
+                class="checkbox-input"
+              />
+              <label for="item" class="checkbox-label">{{ colorNameFormater(item) }}</label>
+            </li>
+          </ul>
+        </div>
+      </app-left>
+      <app-right>
+        <AppHeading
+          title="Кроссовки"
+          :quantity="filterData.length"
+          sortBox="true"
+          @toggleSorting="toggleSorting"
+          :isDesc="isDesc"
+        />
 
-      <!-- <p v-else>Кроссовок не обнаружено</p> -->
-    </app-right>
-  </app-page>
+        <AppProductList :shoes="sortProductByPrice(filterData)" />
+      </app-right>
+    </app-page>
+  </div>
 </template>
 
 <script setup>
@@ -59,7 +71,9 @@ import AppHeading from '@/components/AppHeading.vue'
 import AppBreadcrumbs from '@/components/breadcrumbs/AppBreadcrumbs.vue'
 import AppProductList from '@/components/product/AppProductList.vue'
 import { PATH_SHOES, PATH_CROSSOVKY } from '@/mock/routes'
-import AppFilterBox from '@/components/filters/AppFilterBox.vue'
+import AppLoader from '@/components/AppLoader.vue'
+import { colorNameFormater } from '@/utils/color-name-formater'
+// import AppFilterBox from '@/components/filters/AppFilterBox.vue'
 
 const breadcrumbs = ref([
   { name: 'Главная', path: '/', content: '1' },
@@ -77,6 +91,7 @@ const breadcrumbs = ref([
 
 const crossovky = ref([])
 const isDesc = ref(false)
+const isLoading = ref(false)
 
 const brandList = ref([])
 brandList.value = computed(() => [...new Set(crossovky.value.map((a) => a.brand))])
@@ -96,6 +111,8 @@ const sortProductByPrice = (args) => {
 
 onMounted(async () => {
   try {
+    isLoading.value = true
+
     const { data } = await axios.get('https://vue-xwear-default-rtdb.firebaseio.com/shoes.json')
 
     if (data) {
@@ -108,6 +125,8 @@ onMounted(async () => {
         })
         .filter((item) => item.category === 'crossovky')
     }
+
+    isLoading.value = false
   } catch (error) {
     console.log(error)
   }
@@ -147,8 +166,6 @@ const filterData = computed(() => {
     data = dataColor
   }
 
-  // console.log('dataBrands - ', dataBrands.length)
-  // console.log('dataColor - ', dataColor.length)
   return data
 })
 </script>
@@ -163,7 +180,6 @@ const filterData = computed(() => {
   flex-direction: column;
   gap: 21px;
   width: 100%;
-  /* height: 300px; */
   border: 1px solid var(--gray-light-fivedary);
   border-radius: 5px;
   padding: 25px;
@@ -176,7 +192,50 @@ const filterData = computed(() => {
   text-transform: uppercase;
 }
 .filter-checkbox-list {
+  display: flex;
+  flex-direction: column;
+  gap: 19px;
 }
 .filter-checkbox-item {
+  display: flex;
+  align-items: center;
+  gap: 10px;
+}
+
+.checkbox-input {
+  appearance: none;
+  position: relative;
+  width: 23px;
+  height: 23px;
+  background: var(--white-fourdary);
+  border: 1px solid var(--gray-light-fourdary);
+  border-radius: 3px;
+  transition: 500ms;
+  cursor: pointer;
+}
+
+.checkbox-input::after {
+  content: '';
+  position: absolute;
+  width: 0px;
+  height: 0px;
+  background: var(--blue-primary);
+  background-image: url('../../../../public/icons/icon-checkbox.svg');
+  background-repeat: no-repeat;
+  background-position: center;
+  border-radius: 3px;
+}
+
+.checkbox-input:checked::after {
+  width: 23px;
+  height: 23px;
+}
+
+.checkbox-label {
+  line-height: 20px;
+  font-size: 13px;
+  font-weight: 400;
+  color: var(--black-fourdary);
+  text-transform: capitalize;
 }
 </style>
