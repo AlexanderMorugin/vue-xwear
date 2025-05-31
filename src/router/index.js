c698b3f17b8772a6c45baf0252fc899a2c6aa696
Alexander Morugin <nobilis@bk.ru>
2025-05-31 13:35:59 +0300

    добавил плавную прокрутку страницы вверх

--- src/router/index.js
+++ src/router/index.js
@@ -154,6 +154,13 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { top: 0, behavior: 'smooth' }
+    }
+  },
   // linkActiveClass: 'active',
   // linkExactActiveClass: 'active',
 })
